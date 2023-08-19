9e4c86bac6d6fe3943ccee3843db7e06752d369e
shyamexe <shyamjith38@hotmail.com>
2023-08-19 15:25:54 +0530

    added new work

--- lib/modules/home/widgets/work_tile_widget.dart
+++ lib/modules/home/widgets/work_tile_widget.dart
@@ -2,9 +2,10 @@
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 import 'package:shyamexe/utils/layout_helper.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 class WorkTileWidget extends StatelessWidget {
   final String title;
@@ -16,19 +17,21 @@ class WorkTileWidget extends StatelessWidget {
   final String assetImage;
   final DeviceInfo device;
   final Orientation orientation;
+  final void Function()? onTap;
 
-  WorkTileWidget({
-    Key? key,
-    required this.title,
-    required this.tags,
-    required this.gradientColors,
-    required this.titleColor,
-    required this.tagColor,
-    required this.tagBackGround,
-    required this.assetImage,
-    required this.device,
-    required this.orientation,
-  }) : super(key: key);
+  WorkTileWidget(
+      {Key? key,
+      required this.title,
+      required this.tags,
+      required this.gradientColors,
+      required this.titleColor,
+      required this.tagColor,
+      required this.tagBackGround,
+      required this.assetImage,
+      required this.device,
+      required this.orientation,
+      required this.onTap})
+      : super(key: key);
 
   final ValueNotifier<Matrix4> transformNotifier =
       ValueNotifier(Matrix4.identity());
@@ -57,6 +60,7 @@ class WorkTileWidget extends StatelessWidget {
           tags: tags,
           tagBackGround: tagBackGround,
           tagColor: tagColor,
+          onTap: onTap,
         ),
       );
     } else {
@@ -78,6 +82,7 @@ class WorkTileWidget extends StatelessWidget {
           tags: tags,
           tagBackGround: tagBackGround,
           tagColor: tagColor,
+          onTap: onTap,
         ),
       );
     }
@@ -86,7 +91,7 @@ class WorkTileWidget extends StatelessWidget {
 
 class ChildWidget extends StatelessWidget {
   const ChildWidget({
-    super.key,
+    Key? key,
     required this.transformNotifier,
     required this.gradientColors,
     required this.device,
@@ -96,7 +101,8 @@ class ChildWidget extends StatelessWidget {
     required this.tags,
     required this.tagBackGround,
     required this.tagColor,
-  });
+    this.onTap,
+  }) : super(key: key);
 
   final ValueNotifier<Matrix4> transformNotifier;
   final List<Color> gradientColors;
@@ -107,131 +113,143 @@ class ChildWidget extends StatelessWidget {
   final List<String> tags;
   final Color tagBackGround;
   final Color tagColor;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<Matrix4>(
         valueListenable: transformNotifier,
         builder: (_, transform, __) {
-          return Container(
-              height: LayOutHelper.masterDataCards(context) == 3 ? 450 : 350,
-              margin: EdgeInsets.symmetric(
-                  horizontal:
-                      LayOutHelper.masterDataCards(context) == 2 ? 100 : 50,
-                  vertical: 25),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                boxShadow: transform == Matrix4.identity()
-                    ? []
-                    : [
-                        BoxShadow(
-                            color: gradientColors.first.withOpacity(.8),
-                            spreadRadius: 0,
-                            blurRadius: 10,
-                            offset: const Offset(2, 0))
-                      ],
-                borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: gradientColors,
+          return GestureDetector(
+            onTap: onTap,
+            child: Container(
+                height: LayOutHelper.masterDataCards(context) == 3 ? 450 : 350,
+                margin: EdgeInsets.symmetric(
+                    horizontal:
+                        LayOutHelper.masterDataCards(context) == 2 ? 100 : 50,
+                    vertical: 25),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  boxShadow: transform == Matrix4.identity()
+                      ? []
+                      : [
+                          BoxShadow(
+                              color: gradientColors.first.withOpacity(.8),
+                              spreadRadius: 0,
+                              blurRadius: 10,
+                              offset: const Offset(2, 0))
+                        ],
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: gradientColors,
+                  ),
                 ),
-              ),
-              child: LayoutBuilder(
-                builder: (ctx, size) {
-                  return Stack(
-                    children: [
-                      Positioned(
-                        top: size.maxHeight / 2,
-                        child: AnimatedContainer(
-                          height: size.maxHeight,
-                          width: size.maxWidth,
-                          duration: const Duration(milliseconds: 300),
-                          transform: transform,
-                          child: Center(
-                            child: Container(
-                              decoration: BoxDecoration(boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(.2),
-                                  blurRadius: 50,
-                                  offset: const Offset(0, 24),
-                                  spreadRadius: 30
-                                )
-                              ]),
-                              child: DeviceFrame(
-                                device: device,
-                                isFrameVisible: true,
-                                orientation: Orientation.portrait,
-                                screen: Container(
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: AssetImage(assetImage),
-                                        fit: BoxFit.fill),
+                child: LayoutBuilder(
+                  builder: (ctx, size) {
+                    return Stack(
+                      children: [
+                        Positioned(
+                          top: size.maxHeight / 2,
+                          child: AnimatedContainer(
+                            height: size.maxHeight,
+                            width: size.maxWidth,
+                            duration: const Duration(milliseconds: 300),
+                            transform: transform,
+                            child: Center(
+                              child: Container(
+                                decoration: BoxDecoration(boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.black.withOpacity(.2),
+                                      blurRadius: 50,
+                                      offset: const Offset(0, 24),
+                                      spreadRadius: 30)
+                                ]),
+                                child: DeviceFrame(
+                                  device: device,
+                                  isFrameVisible: true,
+                                  orientation: Orientation.portrait,
+                                  screen: Container(
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(assetImage),
+                                          fit: BoxFit.fill),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: LayOutHelper.masterDataCards(context) == 3
-                            ? Alignment.bottomLeft
-                            : Alignment.topCenter,
-                        child: Padding(
-                          padding: LayOutHelper.masterDataCards(context) == 3
-                              ? const EdgeInsets.all(40)
-                              : const EdgeInsets.symmetric(horizontal: 20),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment:
-                                LayOutHelper.masterDataCards(context) == 3
-                                    ? CrossAxisAlignment.start
-                                    : CrossAxisAlignment.center,
-                            children: [
-                              const SizedBox(
-                                height: 50,
-                              ),
-                              Text(
-                                title,
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.nunito(
-                                  fontSize: 30.0,
-                                  color: titleColor,
+                        Align(
+                          alignment: LayOutHelper.masterDataCards(context) == 3
+                              ? Alignment.bottomLeft
+                              : Alignment.topCenter,
+                          child: Padding(
+                            padding: LayOutHelper.masterDataCards(context) == 3
+                                ? const EdgeInsets.all(40)
+                                : const EdgeInsets.symmetric(horizontal: 20),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment:
+                                  LayOutHelper.masterDataCards(context) == 3
+                                      ? CrossAxisAlignment.start
+                                      : CrossAxisAlignment.center,
+                              children: [
+                                const SizedBox(
+                                  height: 50,
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Wrap(
-                                runSpacing: 12,
-                                spacing: 12,
-                                children: List.generate(
-                                  tags.length,
-                                  (index) => Container(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 4, vertical: 2),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(6),
-                                        color: tagBackGround),
-                                    child: Text(
-                                      tags[index],
-                                      style: GoogleFonts.nunito(
-                                        fontSize: 15.0,
-                                        color: tagColor,
-                                      ),
+                                Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 8),
+                                  decoration: BoxDecoration(
+                                    color: gradientColors.last.withOpacity(.4),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Text(
+                                    title,
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.nunito(
+                                      fontSize: 30.0,
+                                      color: titleColor,
                                     ),
                                   ),
                                 ),
-                              )
-                            ],
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Wrap(
+                                  runSpacing: 12,
+                                  spacing: 12,
+                                  children: List.generate(
+                                    tags.length,
+                                    (index) => Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 4, vertical: 2),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          color: tagBackGround),
+                                      child: Text(
+                                        tags[index],
+                                        style: GoogleFonts.nunito(
+                                          fontSize: 15.0,
+                                          color: tagColor,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  );
-                },
-              ));
+                        )
+                      ],
+                    );
+                  },
+                )),
+          );
         });
   }
 }
--- lib/modules/home/widgets/works_widgets.dart
+++ lib/modules/home/widgets/works_widgets.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'work_tile_widget.dart';
 
@@ -14,6 +15,13 @@ class WorksWidgets extends StatelessWidget {
     return Column(
       children: [
         WorkTileWidget(
+          onTap: () async {
+            await launchUrl(
+                Uri.parse(
+                  'https://pub.dev/packages/scrollable_tab_view',
+                ),
+                mode: LaunchMode.externalNonBrowserApplication);
+          },
           assetImage: 'assets/package.png',
           title: 'scrollable_tab_view package',
           tags: const ['android', 'ios', 'web'],
@@ -28,6 +36,13 @@ class WorksWidgets extends StatelessWidget {
           orientation: Orientation.portrait,
         ),
         WorkTileWidget(
+          onTap: () async {
+            await launchUrl(
+                Uri.parse(
+                  'https://github.com/shyamexe/Dictionary_app_flutter_using_bloc',
+                ),
+                mode: LaunchMode.externalNonBrowserApplication);
+          },
           assetImage: 'assets/dictionary_ss.png',
           title: '1Dictionary app',
           tags: const ['android', 'ios', 'web'],
@@ -42,9 +57,42 @@ class WorksWidgets extends StatelessWidget {
           orientation: Orientation.portrait,
         ),
         WorkTileWidget(
+          onTap: () async {
+            await launchUrl(
+                Uri.parse(
+                  'https://github.com/sajithlaldev/Admin-Dashbord-Demo',
+                ),
+                mode: LaunchMode.externalNonBrowserApplication);
+          },
+          assetImage: 'assets/ecoms.png',
+          title: 'E Commerce Seller App',
+          tags: const ['web'],
+          gradientColors: const [
+            Color(0xffa270b5),
+            Color(0xff5e4169),
+          ],
+          tagBackGround: const Color(0xff33143f),
+          tagColor: Colors.white,
+          titleColor: Colors.white,
+          orientation: Orientation.portrait,
+          // device: Devices.android.mediumTablet,
+          device: Devices.ios.iPadAir4,
+
+        ),
+        WorkTileWidget(
+          onTap: () async {
+            await launchUrl(
+                Uri.parse(
+                  'https://github.com/shyamexe/weather_app',
+                ),
+                mode: LaunchMode.externalNonBrowserApplication);
+          },
           assetImage: 'assets/wether.png',
           title: 'weather app',
-          tags: const ['android', 'ios',],
+          tags: const [
+            'android',
+            'ios',
+          ],
           gradientColors: const [
             Color(0xff33aeaa),
             Color(0xff007991),
