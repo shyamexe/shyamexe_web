439abb421f69a5ef596cbd4412ce72a455f5bbd9
shyamexe <shyamjith38@hotmail.com>
2023-09-08 15:48:16 +0530

    removed unwanded animation for optimazation

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
-import 'package:shyamexe/utils/layout_helper.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:universal_html/html.dart' as html;
 import '../../utils/custom_scroll_behavior.dart';
@@ -22,96 +19,72 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollNotification>(
-      onNotification: (notification) {
-        scrolle.value = percentage(
-            number: notification.metrics.pixels,
-            total: notification.metrics.maxScrollExtent);
-        return false;
-      },
-      child: Scaffold(
-        body: 
-            Column(
-              children: [
-                ValueListenableBuilder<double>(
-                  valueListenable: scrolle,
-                  builder: (_,scrlValue,__) {
-                    return LinearProgressIndicator(
-                      value: scrlValue,
-                      backgroundColor: const Color(0xff14141a),
-                      color: const Color(0xfff9eeff),
-
-                    );
-                  }
-                ),
-                Expanded(
-                  child: ScrollConfiguration(
-                    behavior: CustomScrollBehavior(),
-                    child: RefreshIndicator(
-                      color: const Color(0xfff9eeff),
-                      onRefresh: () async {
-                        html.window.location.reload();
-                      },
-                      child: SingleChildScrollView(
-                        physics: const BouncingScrollPhysics(
-                            // parent: AlwaysScrollableScrollPhysics(),
-                            ),
-                        child: Column(
-                          children: [
-                            const HeaderWidet(),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            const NameBanner(),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            const Center(
-                              child: SizedBox(
-                                width: 1000,
-                                child: WorksWidgets(),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 60, vertical: 30),
-                              child: Row(
-                                children: [
-                                  const Spacer(),
-                                  Text(
-                                    'Say Hi!',
-                                    style: GoogleFonts.nunito(
-                                      fontSize: 15.0,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 12,
-                                  ),
-                                  HoverTextButton(
-                                    title: 'Reach out',
-                                    onTap: () async {
-                                      await launchUrl(
-                                          Uri.parse(
-                                            'mailto:shyamjith38@hotmail.com',
-                                          ),
-                                          mode: LaunchMode
-                                              .externalNonBrowserApplication);
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+    return Scaffold(
+      body: 
+          ScrollConfiguration(
+            behavior: CustomScrollBehavior(),
+            child: RefreshIndicator(
+              color: const Color(0xfff9eeff),
+              onRefresh: () async {
+                html.window.location.reload();
+              },
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(
+                    // parent: AlwaysScrollableScrollPhysics(),
+                    ),
+                child: Column(
+                  children: [
+                    const HeaderWidet(),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const NameBanner(),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Center(
+                      child: SizedBox(
+                        width: 1000,
+                        child: WorksWidgets(),
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 60, vertical: 30),
+                      child: Row(
+                        children: [
+                          const Spacer(),
+                          const Text(
+                            'Say Hi!',
+                            style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 15.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          HoverTextButton(
+                            title: 'Reach out',
+                            onTap: () async {
+                              await launchUrl(
+                                  Uri.parse(
+                                    'mailto:shyamjith38@hotmail.com',
+                                  ),
+                                  mode: LaunchMode
+                                      .externalNonBrowserApplication);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-           
-      ),
+          ),
+         
     );
   }
 }
